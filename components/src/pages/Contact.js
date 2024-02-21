82453f4707e0e3ad711cd98d2f9d68617743a0f8
OktawianM <oktawianmankowski@gmail.com>
2024-02-21 23:53:14 +0100

    function with args passed to other component

--- components/src/pages/Contact.js
+++ components/src/pages/Contact.js
@@ -1,9 +1,10 @@
 function Contact(props) {
 
+    const textToDisplay = "This text is modified by extern function from Head.js file"
     return (
         <>
             <h3>This is Contact Pages with text from Head Page: </h3>
-            {props.xxx()}
+            {props.setTxt(textToDisplay)}
         </>
     )
 }
--- components/src/pages/Head.js
+++ components/src/pages/Head.js
@@ -3,14 +3,14 @@ import Contact from "./Contact";
 function Head() {
 
     const textToPass = "String redirected to Contact.js";
-    const functionToPass = () => {
-        alert("Welcome in fuction");
+    const functionToPass = (sampleTxt) => {
+        return (<h1>{sampleTxt}</h1>);
     }
 
     return (
         <>
             <h2>This is Head Page</h2>
-            <Contact xxx={functionToPass} />
+            <Contact setTxt={functionToPass} />
         </>
     )
 }
