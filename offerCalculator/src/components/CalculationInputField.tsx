e0eef804e61933909162521c06e6b701184a1cb7
OktawianM <oktawianmankowski@gmail.com>
2025-04-11 15:11:02 +0200

    Minor logic mods

--- offerCalculator/src/components/CalculationInputField.tsx
+++ offerCalculator/src/components/CalculationInputField.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   calculationDataType,
   inputFieldProps,
@@ -6,21 +7,32 @@ import {
 export default function CalculationInputField(
   props: inputFieldProps
 ): JSX.Element {
+  const [value, setValue] = useState(
+    props.fieldData.defaultValue
+      ? props.fieldData.defaultValue
+      : props.fieldData.value
+  );
+
+  useEffect(() => {
+    props.fieldData.defaultValue
+      ? props.setCalculatorData((prev: calculationDataType) => ({
+          ...prev,
+          [`${props.fieldData.id}`]: props.fieldData.defaultValue,
+        }))
+      : props.setCalculatorData((prev: calculationDataType) => ({
+          ...prev,
+          [`${props.fieldData.id}`]: value,
+        }));
+  }, [value]);
+
   return (
     <div className="lg:w-1/2 text-white mt-5 ">
       <input
         className="w-full rounded-md border-1 bg-white/5 text-white sm:text-sm/12 h-10 bottom-0 mt-5"
         type="text"
-        value={
-          props.fieldData.value
-            ? props.fieldData.value
-            : props.fieldData.defaultValue
-        }
+        value={value}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          props.setCalculatorData((prev: calculationDataType) => ({
-            ...prev,
-            [`${props.fieldData.id}`]: event.target.value,
-          }));
+          setValue(event.target.value);
         }}
       />
       <h4>{props.fieldData.description}</h4>
